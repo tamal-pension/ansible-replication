1ea20d848dadcb35748d42ae1ab8f55be3c7733a
asisrae33 <israel.a@anopa.co.il>
2024-12-13 12:02:36 +0200

    Update run.sh
    add RAM configuration as percent of the available RAM

--- roles/pension/files/bin/run.sh
+++ roles/pension/files/bin/run.sh
@@ -44,6 +44,8 @@ JMX_OPTS=(
   "-Dcom.sun.management.jmxremote.local.only=false"
   "-Dcom.sun.management.jmxremote.authenticate=false"
   "-Dcom.sun.management.jmxremote.ssl=false"
+  "-XX:MaxRAMPercentage=80.0"
+  "-XX:InitialRAMPercentage=20.0"
 )
 
 # Module and export options for Java
